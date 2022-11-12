920b6478ce56bfc129d75eca9766e058e91d0870
skaiterskull <hendra.here@yahoo.com>
2022-11-12 13:29:41 +1100

    added rest.http and fetch user

--- server.js
+++ server.js
@@ -14,47 +14,47 @@ mongoDB();
 app.use(express.json());
 app.use(urlencoded({ extended: true }));
 
-app.get("/", async (req, res, next) => {
-  // try {
-  //   console.log(req.body);
-  //   const user = await createUser(req.body);
-
-  //   user._id
-  //     ? res.json({
-  //         status: "success",
-  //         message: "User has been created.",
-  //       })
-  //     : res.json({
-  //         status: "error",
-  //         message: "Unable to create account, please contact administrator.",
-  //       });
-  // } catch (error) {
-  //   if (error.message.includes("E11000 duplicate key error collection")) {
-  //     error.status = 200;
-  //     error.message = "Email already exist";
-  //   }
-  //   next(error);
-  // }
-
+app.post("/", async (req, res, next) => {
   try {
-    const result = await fetchUser();
+    console.log(req.body);
+    const user = await createUser(req.body);
 
-    if (result.length) {
-      return res.json({
-        status: "success",
-        result,
-      });
-    }
-
-    res.json({ status: "error", message: "user not found" });
+    user._id
+      ? res.json({
+          status: "success",
+          message: "User has been created.",
+        })
+      : res.json({
+          status: "error",
+          message: "Unable to create account, please contact administrator.",
+        });
   } catch (error) {
-    if (error) {
-      error.status = 500;
-      error.message = "Internal server error";
+    if (error.message.includes("E11000 duplicate key error collection")) {
+      error.status = 200;
+      error.message = "Email already exist";
     }
     next(error);
   }
-});
+
+//   try {
+//     const result = await fetchUser();
+
+//     if (result.length) {
+//       return res.json({
+//         status: "success",
+//         result,
+//       });
+//     }
+
+//     res.json({ status: "error", message: "user not found" });
+//   } catch (error) {
+//     if (error) {
+//       error.status = 500;
+//       error.message = "Internal server error";
+//     }
+//     next(error);
+//   }
+// });
 
 app.listen(PORT, (error) => {
   console.log(`Server is ready at http://localhost:${PORT}`);
