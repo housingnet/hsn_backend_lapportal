16906aa460aa1520ac8ee49fb1d00074c96ad776
skaiterskull <hendra.here@yahoo.com>
2022-11-21 11:57:36 +1100

    added get-user api mssql

--- server.js
+++ server.js
@@ -59,6 +59,54 @@ app.post("/add-user", async (req, res) => {
   }
 });
 
+app.post("/get-user", async (req, res) => {
+  try {
+    const pool = new sql.ConnectionPool({
+      authentication: {
+        options: {
+          userName: process.env.DATABASE_USERNAME,
+          password: process.env.DATABASE_PASSWORD,
+        },
+        type: "default",
+      },
+      database: process.env.DATABASE_NAME,
+      server: process.env.DATABASE_SERVER,
+      options: {
+        encrypt: true,
+      },
+    });
+
+    pool
+      .connect()
+      .then(() => {
+        //simple query
+        pool
+          .request()
+          .query(
+            `SELECT * FROM person WHERE email='${req.body.email}'`,
+            (err, result) => {
+              sql.close();
+              return res.json({
+                status: "success",
+                message: result.recordset,
+              });
+            }
+          );
+      })
+      .catch((error) => {
+        return res.json({
+          status: "error",
+          message: error.message,
+        });
+      });
+  } catch (error) {
+    console.log(error);
+    return res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
 app.listen(PORT, (error) => {
   console.log(`Server is ready at http://localhost:${PORT}`);
 });
