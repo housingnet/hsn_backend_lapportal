04495a6737507dc84f7ad2600a9c782da7c804e5
housingnnet <lapdesk@housingnet.com.au>
2022-12-04 13:07:40 +1100

    add login api

--- src/helpers/bcrypt.js
+++ src/helpers/bcrypt.js
@@ -4,3 +4,7 @@ export const hashPassword = (password) => {
   var salt = bcrypt.genSaltSync(5);
   return bcrypt.hashSync(password, salt);
 };
+
+export const comparePassword = (password, hashedPassword) => {
+  return bcrypt.compareSync(password, hashedPassword);
+};
--- src/routers/accountRoute.js
+++ src/routers/accountRoute.js
@@ -1,6 +1,6 @@
 import express from "express";
 import sql from "mssql";
-import { hashPassword } from "../helpers/bcrypt.js";
+import { comparePassword, hashPassword } from "../helpers/bcrypt.js";
 
 const Router = express.Router();
 
@@ -31,6 +31,49 @@ Router.post("/register", async (req, res) => {
         message: "Something went wrong, please try again!",
       });
     }
+  } catch (error) {
+    console.log(error);
+
+    // check if user already exist, passing different message to user
+    if (error.message.includes("Violation of PRIMARY KEY")) {
+      return res.json({
+        status: "error",
+        message: "Please try using different email",
+      });
+    }
+
+    // default error message if error exist
+    return res.json({
+      status: "error",
+      message: error.message,
+    });
+  }
+});
+
+Router.post("/login", async (req, res) => {
+  try {
+    // get value from body
+    const { email, password } = req.body;
+
+    //check if user exist
+    const result = await sql.query`SELECT * FROM Users WHERE email=${email}`;
+    if (result.recordset.length > 0) {
+      //Compare password
+      const hashedPassword = result.recordset[0].Password;
+      const passwordMatched = comparePassword(password, hashedPassword);
+      if (passwordMatched) {
+        return res.json({
+          status: "success",
+          message: "Login successful",
+        });
+      }
+    }
+
+    // If login failed
+    return res.json({
+      status: "error",
+      message: "Invalid credential",
+    });
   } catch (error) {
     console.log(error);
 
